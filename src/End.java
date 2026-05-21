521a4a357722d4bbd9eb162677ea13363fac5279
Md Fardin Islam (Rijby Sarkar) <mdfardinislamrijby@gmail.com>
2026-05-21 16:27:14 +0600

    Updated End class

--- src/End.java
+++ src/End.java
@@ -1,12 +1,9 @@
 package game;
 
 public abstract class End {
-    // Final key variable naming id system (Criterion 4)
     protected final String gameId;
-
     public End(String id) {
         this.gameId = id;
     }
-
     public abstract void finish();
 }
