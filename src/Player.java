6caf28353e3e5782f4c84e1457a079a86d08d665
rijbysarkar390 <mdfardinislamrijby@gmail.com>
2026-05-21 13:56:26 +0600

    Added player class

---
+++ src/Player.java
@@ -0,0 +1,33 @@
+package game;
+public class Player extends End {
+    private String name;
+    private String sign;
+    private int score;
+    public static int countPlayers = 0;
+    public Player(String id, String name, String sign) {
+        super(id); 
+        this.name = name;
+        this.sign = sign;
+        this.score = 0;
+        countPlayers++;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSign() {
+        return sign;
+    }
+    public int getScore() {
+        return score;
+    }
+    public void updateScore() {
+        this.score++;
+    }
+    @Override
+    public void finish() {
+        this.score = 0;
+    }
+}
