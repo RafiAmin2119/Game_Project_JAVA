a9a0c00599b529fe8ad994da7813904dabd6bd27
Rafi Amin <mohammadrafi2119@gmail.com>
2026-05-23 11:49:00 +0600

    integrate unique red theme, impact font and custom starting screen triggers

--- src/game/Window.java
+++ src/game/Window.java
@@ -1,16 +1,25 @@
+
 package game;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+
 public class Window extends Start implements Restart {
     private JButton[][] boxes = new JButton[3][3];
     private Player p1;
@@ -19,11 +28,24 @@ public class Window extends Start implements Restart {
     private JLabel turnLabel;
     private JLabel scoreLabel;
     private MainLogic engineLogic;
+
+    private boolean isGameStarted = false;
+    private Image startImg;
+    
+    private Font customFont = new Font("Impact", Font.PLAIN, 14); 
+    private final Color themeRed = new Color(220, 20, 20); 
+
     public Window(String id) {
         super(id, "Tic Tac Toe"); 
         p1 = new Player("P-1", "Rafi", "X");
         p2 = new Player("P-2", "Rijby", "O");
         activeTurn = p1;
+
+        try {
+            startImg = new ImageIcon(getClass().getResource("starting.png")).getImage();
+        } catch (Exception e) {
+            System.out.println("Starting image load failed!");
+        }
 
         engineLogic = new MainLogic(boxes);
         initLayout();
@@ -31,36 +53,58 @@ public class Window extends Start implements Restart {
 
     @Override
     public void initLayout() {
-        this.setSize(450, 500);
+        this.setSize(600, 600); 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
+        this.setResizable(false);
+
+        this.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (!isGameStarted && (e.getKeyCode() == KeyEvent.VK_A)) {
+                    isGameStarted = true;
+                    showGameInterface(); 
+                }
+            }
+        });
+        this.setFocusable(true);
+
+        StartPanel startPanel = new StartPanel();
+        this.add(startPanel);
+
+        System.out.println("Match engine started. Active user instances created: " + Player.countPlayers);
+        this.setVisible(true);
+    }
+
+    private void showGameInterface() {
+        this.getContentPane().removeAll(); 
         this.setLayout(new BorderLayout());
 
         JPanel topPanel = new JPanel(new GridLayout(2, 1));
-        topPanel.setBackground(new Color(139, 0, 0)); // Matching crimson theme color shading
+        topPanel.setBackground(themeRed); 
 
         turnLabel = new JLabel(activeTurn.getName() + "'s Turn", SwingConstants.CENTER);
-        turnLabel.setFont(new Font("Arial", Font.BOLD, 28));
-        turnLabel.setForeground(Color.BLACK);
+        turnLabel.setFont(customFont.deriveFont(Font.BOLD, 36f)); 
+        turnLabel.setForeground(Color.WHITE);
 
         scoreLabel = new JLabel(p1.getName() + " : " + p1.getScore() + "  |  " + p2.getName() + " : " + p2.getScore(), SwingConstants.CENTER);
-        scoreLabel.setFont(new Font("Arial", Font.PLAIN, 16));
-        scoreLabel.setForeground(Color.BLACK);
+        scoreLabel.setFont(customFont.deriveFont(Font.PLAIN, 18f));
+        scoreLabel.setForeground(Color.WHITE);
 
         topPanel.add(turnLabel);
         topPanel.add(scoreLabel);
         this.add(topPanel, BorderLayout.NORTH);
 
         JPanel centerPanel = new JPanel(new GridLayout(3, 3));
-        centerPanel.setBackground(Color.DARK_GRAY);
+        centerPanel.setBackground(themeRed); 
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 final int r = i;
                 final int c = j;
                 boxes[r][c] = new JButton("");
-                boxes[r][c].setFont(new Font("Arial", Font.BOLD, 70));
-                boxes[r][c].setForeground(new Color(178, 34, 34)); 
+                boxes[r][c].setFont(customFont.deriveFont(Font.BOLD, 90f)); 
+                boxes[r][c].setForeground(themeRed); 
                 boxes[r][c].setBackground(Color.WHITE);
                 boxes[r][c].setFocusPainted(false);
                 boxes[r][c].addActionListener(new ActionListener() {
@@ -76,7 +120,6 @@ public class Window extends Start implements Restart {
                                 JOptionPane.showMessageDialog(null, "Match finished! The layout is a Draw!");
                                 refreshGame();
                             } else {
-                            
                                 activeTurn = (activeTurn == p1) ? p2 : p1;
                                 turnLabel.setText(activeTurn.getName() + "'s Turn");
                             }
@@ -87,10 +130,10 @@ public class Window extends Start implements Restart {
             }
         }
         this.add(centerPanel, BorderLayout.CENTER);
-        
-        System.out.println("Match engine started. Active user instances created: " + Player.countPlayers);
-        this.setVisible(true);
+        this.revalidate();
+        this.repaint();
     }
+
     @Override
     public void refreshGame() {
         for (int i = 0; i < 3; i++) {
@@ -101,5 +144,30 @@ public class Window extends Start implements Restart {
         activeTurn = p1; 
         turnLabel.setText(activeTurn.getName() + "'s Turn");
         scoreLabel.setText(p1.getName() + " : " + p1.getScore() + "  |  " + p2.getName() + " : " + p2.getScore());
+    }
+
+    private class StartPanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            if (startImg != null) {
+                g2d.drawImage(startImg, 0, 0, 600, 600, this);
+            } else {
+                g2d.setColor(Color.WHITE);
+                g2d.fillRect(0, 0, 600, 600);
+            }
+
+            g2d.setFont(customFont.deriveFont(Font.BOLD, 30f));
+            g2d.setColor(themeRed);
+
+            String text = "Press A to start the game";
+            int stringWidth = g2d.getFontMetrics().stringWidth(text);
+            int x = (600 - stringWidth) / 2;
+            
+            g2d.drawString(text, x, 500); 
+        }
     }
 }
